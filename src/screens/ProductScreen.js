c07f1c91ec890d9736a3b81773bc4a2bec0a7a16
Chang Ki Heon <changkiheon@naver.com>
2024-01-07 21:40:35 +0900

    linkContainer

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+
+function ProductScreen() {
+    return (
+        <div>
+            <h1>Product</h1>
+            <Row>
+                <Col>
+                    Test
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default ProductScreen
